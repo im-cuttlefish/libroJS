04dcc862230cb8d4702263bfe3814f2f359a54c1
im-cuttlefish <firefly.squid.ink@gmail.com>
2018-12-31 00:53:28 +0900

    removed unnecessary files.

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,41 @@ import { Player } from "./screen";
 import { initMenu } from "./menu";
 import { StoryData } from "./interface";
 
-(async () => {
-  const story_data = await Axios.get(
-    "http://127.0.0.1:8080/src/assets/scenario.json"
-  ).then(response => {
+const getDate = () => {
+  const date = new Date();
+  return `${date.getMonth() + 1}/${date.getDate()}`;
+};
+
+const getStory = async (url: string) =>
+  await Axios.get(url).then(response => {
     return Promise.resolve(response.data as StoryData);
   });
 
-  const width = story_data.config.width;
-  const height = story_data.config.height;
+(async () => {
+  const url = "http://127.0.0.1:8080/src/assets/init.json";
+
+  const story_data = await getStory(url);
+
+  const config = story_data.config;
+  const title = config.title;
+  const width = config.width;
+  const height = config.height;
 
   const root = new DisplayRoot(width, height);
   const player = new Player(root);
+  const date = getDate();
 
-  initMenu();
+  initMenu({ date, title, url });
 
   const render = async (story_data: StoryData) => {
     await player.init(story_data);
+
     const url = await player.start();
-    const next = await Axios.get(url).then(response =>
-      Promise.resolve(response.data as StoryData)
-    );
+    const next = await getStory(url);
+    const title = config.title;
+    const date = getDate();
+
+    initMenu({ date, url, title });
     await render(next);
   };
 
